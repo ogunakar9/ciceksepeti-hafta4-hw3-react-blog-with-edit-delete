0fdd976c7e69bda0a32899f4e33a45112d76ae91
ogunakar9 <ogunakar09@gmail.com>
2021-09-28 22:32:24 +0300

    add header skeleton

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import "./scss/main.scss";
 import CardContainer from "./components/CardContainer";
 import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => {
         setPosts(data);
+        setFilteredPosts(data);
       });
   }, []);
 
+  //TODO: filter items
+  const filterPosts = () => {
+
+  }
+
   return (
-    <>
+    <div className="main-container">
+      <Header />
       <CardContainer posts={posts}/>
-    </>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header__container">
+            <div className="header__logo">logo</div>
+            <div className="header__input-container"><input></input></div>
+            <div className="header__profile-pic">profile pic</div>
+        </div>
+    )
+}
+
+export default Header;
