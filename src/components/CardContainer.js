ea4f3c4343d30ffa8f8ed6efce9b7914f425ea3a
ogunakar9 <ogunakar09@gmail.com>
2021-09-28 21:50:14 +0300

    edit readme file

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -2,7 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardContainer = ({posts}) => {
-    // console.log(posts);
   return (
     <div className="cards-main-container">
       {posts.map((post) => {
