beae2e54067e4e6c575f5f8a2f5848b35c96a252
ogunakar9 <ogunakar09@gmail.com>
2021-10-01 23:37:20 +0300

    edit header to have icons and link to patika

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,12 +12,13 @@ const Footer = () => {
   const gitHubLink = "https://github.com/ogunakar9";
   return (
     <div className="footer__container">
-      <div className="header__logo">logo</div>
       <div className="footer__github-section">
         <span>Created by Ogun Akar</span>
-        <GoMarkGithub className="footer__github-icon" onClick={() => openLink(gitHubLink)} />
+        <GoMarkGithub
+          className="footer__github-icon"
+          onClick={() => openLink(gitHubLink)}
+        />
       </div>
-      <div className="header__profile-pic">profile pic</div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,51 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { FaBlogger } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa";
 
-const Header = ({filterPosts, setIsFiltering, filteredPosts}) => {
-    const [val, setVal] = useState("");
+const Header = ({ filterPosts, setIsFiltering, filteredPosts }) => {
+  const [val, setVal] = useState("");
 
-    const handleSearch = (e) => {
-        setIsFiltering(true);
-        setVal(e.target.value);
-        filterPosts(e.target.value);
+  const handleSearch = (e) => {
+    setIsFiltering(true);
+    setVal(e.target.value);
+    filterPosts(e.target.value);
 
-        if (e.target.value === "") {
-            setIsFiltering(false);
-        }
+    if (e.target.value === "") {
+      setIsFiltering(false);
     }
+  };
 
-    return (
-        <div className="header__container">
-            <div className="header__logo">logo</div>
-            <div className="header__input-container">
-                <input type="text" 
-                placeholder="Search" 
-                value={val} 
-                onChange={(e) => handleSearch(e)}
-                />
-                </div>
-            <div className="header__profile-pic">profile pic</div>
-        </div>
-    )
-}
+  const openLink = (url) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) {
+      newWindow.opener = null;
+    }
+  };
+
+  const patikaLink = "https://www.patika.dev/";
+
+  return (
+    <div className="header__container">
+      <div className="header__logo">
+        <FaBlogger className="header__logo-icon" />
+        <p>Blog Details</p>
+      </div>
+      <div className="header__input-container">
+        <input
+          type="text"
+          placeholder="Search"
+          value={val}
+          onChange={(e) => handleSearch(e)}
+        />
+      </div>
+      <div className="header__profile-pic">
+        <FaUserTie
+          className="header__profile-icon"
+          onClick={() => openLink(patikaLink)}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
